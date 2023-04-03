d52a93fc43207fbdcd0a0c30081afa80808e2161
EunbiAn <qldms133@naver.com>
2023-04-03 13:17:42 +0900

    todo

---
+++ src/Todo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+
+
+function Todo(props) {
+    const item = props.item;
+
+    return (
+        <ListItem>
+            <Checkbox
+                checked={item.done}
+            />
+            <ListItemText>
+                <InputBase
+                    inputProps={{ "aria-label": "naked" }}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}
+
+                />
+            </ListItemText>
+
+
+        </ListItem>
+    );
+}
+
+export default Todo;
