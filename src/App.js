1929aef84f5238fa5dac132a6188a604e887ee72
EunbiAn <qldms133@naver.com>
2023-04-04 11:06:40 +0900

    7. 리액트로 '안녕하세요' 출력하기

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import Todo from './Todo';
-import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core";
+import React from "react";
+import ReactDOM from "react-dom";
 import './App.css';
+import Hello from "./Hello";
 
-function App() {
-  const [items, setItems] = useState([
-    { id: 0, title: "Todo 1 ", done: true },
-    { id: 1, title: "Todo 2 ", done: false },
-  ]);
-  
-  const [todoItems, setTodoItems] = useState("");
-
-  // add 함수 추가
-  const add = (item) => {
-    const thisItems = items;
-    item.id = "ID-" + thisItems.length;//key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    setItems(thisItems);
-    setTodoItems(items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
-        <List>
-          {items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
-          ))}
-        </List>
-      </Paper>
-    ));
-    console.log(items);
-  }
-
-  useEffect(() => {
-    setTodoItems(items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
-        <List>
-          {items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
-          ))}
-        </List>
-      </Paper>
-    ))
-  }, []);
-
-  return (
-    <div className="App">
-      <Container maxWidth="md">
-        <AddTodo add={add} />
-        <div className="TodoList">{todoItems}</div>
-      </Container>
-    </div>
-  );
-
+class App extends React.Component {
+    render(){
+        return (
+            <div className="App">
+                <Hello />
+            </div>
+        )
+    }
 }
-
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render(){
+        return (
+            <div className="Hello">
+                <label for="hello">안녕하세욤</label>
+            </div>
+        )
+    }
+}
+export default Hello;
