151016bce2c2b1c3d45c8e46f4a5ef1a2ac907ce
EunbiAn <qldms133@naver.com>
2023-04-04 11:32:55 +0900

    9. 리액트에서 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './App.css';
-import Hello from "./Hello";
 import Todo from "./Todo";
+import {Paper, List} from "@material-ui/core";
 
 class App extends React.Component {
     constructor(props){ // 매개변수 props 생성자
         super(props);   // props 초기화
         this.state={
-            item: {id:0, title:"Hello world!", done:true}
+            items: [
+                {id:0, title:"Todo 1", done:false},
+                {id:1, title:"Todo 2", done:false}
+            ]
         }
     }
     render(){
-        return ( //   매개변수 item에 변수명/값을 전달
+        // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러 개 생성
+
+        var todoItems = this.state.items.length > 0 &&(
+            <Paper style={{margin:16}}>
+                <List>
+                    {this.state.items.map((item, idx)=>(
+                        <Todo item={item} ket={item.id} />
+                    ))}
+                </List>
+            </Paper>
+        );
+        
+        return ( 
             <div className="App">
-                <Todo item ={this.state.item}/>
+               {todoItems}
             </div>
         )
     }
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
 
 class Todo extends React.Component {
     constructor(props){ // 매개변수 props 생성자
@@ -9,16 +9,22 @@ class Todo extends React.Component {
         }
     }
     render(){
+        const item = this.state.item;
         return ( 
-            <div className="Todo">
-                <input 
-                    type="checkbox"
-                    id={this.state.item.id}
-                    name={this.state.item.id}
-                    checked={this.state.item.done}
-                />
-                <label for={this.state.item.id}>{this.state.item.title}</label>
-            </div>
+            <ListItem>
+                <Checkbox checked={item.done}/>
+                <ListItemText>
+                    <InputBase
+                        inputProps={{"aria-label":"naked"}}
+                        type="text"
+                        id={item.id}
+                        name={item.id}
+                        value={item.title}
+                        multiline={true}
+                        fullWidth={true}
+                    />
+                </ListItemText>
+              </ListItem>
         )
     }
 }
