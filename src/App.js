baceb4eb28951c63456e6713367622c275742785
EunbiAn <qldms133@naver.com>
2023-04-07 09:46:18 +0900

    App.js function으로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import './App.css';
 import Todo from "./Todo";
@@ -6,56 +6,55 @@ import {Paper, List, Container, AppBar} from "@material-ui/core";
 import AddTodo from "./AddTodo";
 import { call, signout } from "./service/ApiService";
 import { Button,  Grid, TextField, Typography, Toolbar } from '@material-ui/core';
-class App extends React.Component {
-    constructor(props){ // 매개변수 props 생성자
-        super(props);   // props 초기화
-        this.state={
-            items: [],
-            loading: true,
-        };
-    }
+const App = ()=>{ 
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetchUsers();
+    }, [items]);
+
+  const fetchUsers = async (currentPage) => {
+    call("/todo","GET",null).then((response)=>{
+           setItems(response.data);
+           setLoading(false);
+        });
+  };
 
     // add 함수 -> item 추가 함수
-    add=(item)=>{
+   const add=(item)=>{
         call("/todo","POST",item).then((response)=>{
-            this.setState({items: response.data});
+            setItems(response.data);
         })
-    }
+    };
 
     // delete 함수
-    delete=(item)=>{
+  const deleteItems=(item)=>{
         call("/todo","DELETE",item).then((response)=>{
-            this.setState({items: response.data});
+            setItems(response.data);
         })
-    }
+    };
 
     // update
-    update=(item)=>{
+  const  updateItems=(item)=>{
         call("/todo","PUT",item).then((response)=>{
-            this.setState({items: response.data});
+            setItems(response.data);
         })
-    }
+    };
 
-    componentDidMount(){
-        call("/todo","GET",null).then((response)=>{
-            this.setState({items: response.data, loading:false});
-        });
-    }
 
-    render(){
         // 자바스크립트가 제공하는 map 함수를 이용해서 배열을 반복해 <Todo /> 컴포넌트를 여러 개 생성
 
-        var todoItems = this.state.items.length > 0 &&(
+        var todoItems = items.length > 0 &&(
             <Paper style={{margin:16}}>
                 <List>
-                    {this.state.items.map((item, idx)=>(
-                        <Todo item={item} ket={item.id} delete={this.delete} update={this.update}/>
+                    {items.map((item, idx)=>(
+                        <Todo item={item} ket={item.id} delete={deleteItems} update={updateItems}/>
                     ))}
                 </List>
             </Paper>
         );
         
-        var navigationBar=(
+     var navigationBar=(
             <AppBar position="static">
                 <Toolbar>
                     <Grid justify="space-between" Container>
@@ -75,7 +74,7 @@ class App extends React.Component {
             <div>
                 {navigationBar}
                 <Container maxWidth="md">
-                    <AddTodo add={this.add} />
+                    <AddTodo add={add} />
                     <div className="TodoList">{todoItems}</div>
                 </Container>
             </div>
@@ -85,16 +84,15 @@ class App extends React.Component {
         var loadingPage = <h1>로딩중..</h1>
         var content = loadingPage;
 
-        if(!this.state.loading) {
+        if(!loading) {
             content = todoListPage;
         }
         // 생성된 컴포넌트 JPX 리턴
         return ( 
             <div className="App">
                {content}
-              
             </div>
         )
-    }
+    
 }
 export default App;
